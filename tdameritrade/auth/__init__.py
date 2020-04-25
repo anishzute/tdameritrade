5a0c43a2b5a0f3282f5efb902875f993c6caf1e8
anishzute <anishzute@gmail.com>
2020-04-24 22:21:10 -0500

    changed auth init to allow headless authentication from google colab and WSL

--- tdameritrade/auth/__init__.py
+++ tdameritrade/auth/__init__.py
@@ -13,28 +13,44 @@ def authentication(client_id, redirect_uri, tdauser=None, tdapass=None):
     client_id = client_id + '@AMER.OAUTHAP'
     url = 'https://auth.tdameritrade.com/auth?response_type=code&redirect_uri=' + up.quote(redirect_uri) + '&client_id=' + up.quote(client_id)
 
+    # options = webdriver.ChromeOptions()
+
+    # if sys.platform == 'darwin':
+    #     # MacOS
+    #     if os.path.exists("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"):
+    #         options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    #     elif os.path.exists("/Applications/Chrome.app/Contents/MacOS/Google Chrome"):
+    #         options.binary_location = "/Applications/Chrome.app/Contents/MacOS/Google Chrome"
+    # elif 'linux' in sys.platform:
+    #     # Linux
+    #     options.binary_location = which('google-chrome') or which('chrome') or which('chromium')
+
+    # else:
+    #     # Windows
+    #     if os.path.exists('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'):
+    #         options.binary_location = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
+    #     elif os.path.exists('C:/Program Files/Google/Chrome/Application/chrome.exe'):
+    #         options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+
+    # chrome_driver_binary = which('chromedriver') or "/usr/local/bin/chromedriver"
+    # driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+
+#############################################
+#Allows running headless on google colab
+
+
+    sys.path.insert(0,'/usr/lib/chromium-browser/chromedriver')
+    from selenium import webdriver
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    chrome_driver_binary = which('chromedriver') or "/usr/lib/chromium-browser/chromedriver"
+    driver = webdriver.Chrome('chromedriver', options=options)
 
-    if sys.platform == 'darwin':
-        # MacOS
-        if os.path.exists("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"):
-            options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-        elif os.path.exists("/Applications/Chrome.app/Contents/MacOS/Google Chrome"):
-            options.binary_location = "/Applications/Chrome.app/Contents/MacOS/Google Chrome"
-    elif 'linux' in sys.platform:
-        # Linux
-        options.binary_location = which('google-chrome') or which('chrome') or which('chromium')
 
-    else:
-        # Windows
-        if os.path.exists('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'):
-            options.binary_location = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
-        elif os.path.exists('C:/Program Files/Google/Chrome/Application/chrome.exe'):
-            options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
 
-    chrome_driver_binary = which('chromedriver') or "/usr/local/bin/chromedriver"
-    driver = webdriver.Chrome(chrome_driver_binary, chrome_options=options)
-
+##############################################
     driver.get(url)
 
     # Set tdauser and tdapass from environemnt if TDAUSER and TDAPASS environment variables were defined
